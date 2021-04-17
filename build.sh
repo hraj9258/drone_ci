ac7c658f58d7f8a6e48e1cd9ab6d22958bf2dc03
Himanshu Raj <himanshurajkush@gmail.com>
2021-04-17 20:01:38 +0530

    Reverted my lol commit

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Just a basic script U can improvise lateron asper ur need xD 
+
+mkdir -p /tmp/recovery
+
+cd /tmp/recovery
+
+sudo apt install git -y
+
+repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 -g default,-device,-mips,-darwin,-notdefault 
+
+repo sync -j$(nproc --all)
+
+git clone https://github.com/hraj9258/twrp_phoenix -b test-oss device/xiaomi/phoenix
+
+rm -rf out
+
+. build/envsetup.sh && lunch omni_phoenix-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
+
+# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+
+cd out/target/product/phoenix
+
+sudo zip -r9 TWRP-phoenix-oos.zip recovery.img
+
+curl -sL https://git.io/file-transfer | sh 
+
+./transfer wet *.zip
+
+./transfer wet recovery.img
