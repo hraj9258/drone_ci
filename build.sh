b9ff464ab960e94e99cb6c071b5fb88bc7e1dc72
HemantJabalpuri <hemanthjabalpuri@gmail.com>
2021-06-28 10:35:43 +0530

    Build: phoenix twrp a11
    Improved the whole build.sh

--- build.sh
+++ build.sh
@@ -2,30 +2,39 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-mkdir -p /tmp/recovery
+MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11"
+DEVICE=phoenix
+DT_LINK="https://github.com/hraj9258/twrp_phoenix -b a11"
+DT_PATH=device/xiaomi/$DEVICE
 
-cd /tmp/recovery
+echo " ===+++ Setting up Build Environment +++==="
+apt install openssh-server -y
+apt update --fix-missing
+apt install openssh-server -y
+mkdir ~/twrp11 && cd ~/twrp11
 
-sudo apt install git -y
+echo " ===+++ Syncing Recovery Sources +++==="
+repo init --depth=1 -u $MANIFEST
+repo sync
+repo sync
+git clone --depth=1 $DT_LINK $DT_PATH
 
-repo init --depth=1  --partial-clone --clone-filter=blob:limit=10M -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11 -g default,-device,-mips,-darwin,-notdefault 
-
-repo sync -j$(nproc --all)
-
-git clone https://github.com/hraj9258/twrp_phoenix -b a11 device/xiaomi/phoenix
-
-rm -rf out
-
-. build/envsetup.sh && lunch omni_phoenix-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
+echo " ===+++ Building Recovery +++==="
+export ALLOW_MISSING_DEPENDENCIES=true
+. build/envsetup.sh
+echo " source build/envsetup.sh done"
+lunch twrp_${DEVICE}-eng || abort " lunch failed with exit status $?"
+echo " lunch twrp_${DEVICE}-eng done"
+mka recoveryimage || abort " mka failed with exit status $?"
+echo " mka recoveryimage done"
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+echo " ===+++ Uploading Recovery +++==="
+version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
+OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 
-cd out/target/product/phoenix
+cd out/target/product/$DEVICE
+mv recovery.img ${OUTFILE%.zip}.img
+zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 
-sudo zip -r9 TWRP-phoenix-a11.zip recovery.img
-
-curl -sL https://git.io/file-transfer | sh 
-
-./transfer wet *.zip
-
-./transfer wet recovery.img
+curl -T $OUTFILE https://oshi.at
